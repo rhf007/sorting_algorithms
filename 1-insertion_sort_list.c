2f0f11fb7ef570dac0780b4be6b29e92ca238844
rhf007 <rahofahazem512@gmail.com>
2023-09-22 13:12:23 -0700

    Easy but with TONS of thinking

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,47 +6,39 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	int i, j, key;
 	listint_t *current, *prv, *tmp;
 
 	if (list == NULL || *list == NULL)
+	{
 		return;
+	}
+
 	current = *list;
-	while (current = current->next)
+
+	while ((current = current->next))
 	{
 		tmp = current;
+
 		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
 		{
 			prv = tmp->prev;
-			/**
-			 * do i have a next?
-			 * yes: make it point to my previous
-			 */
+
 			if (tmp->next)
+			{
 				tmp->next->prev = prv;
+			}
 
-			/**
-			 * does my previous have another previous before it?
-			 * yes: make it point to my next
-			 */
 			if (prv->prev)
+			{
 				prv->prev->next = tmp->next;
+			}
 
-			/**
-			 * no: then the previous of my previous is null which means
-			 * the list pointer/head should move to point to me, so that i'm the head
-			 */
 			else
+			{
 				*list = tmp;
+			}
 
-			/**
-			 * now that im done checking my boundaries, make my previous point to my
-			 * next & make my prev pointer point to the same thing my previous' prev pointer
-			 * is pointing to
-			 * make my next point to previous (hence the swap, my previous now comes after me)
-			 * make my previous prev point to me
-			 */
-			prev->next = tmp->next;
+			prv->next = tmp->next;
 			tmp->prev = prv->prev;
 			tmp->next = prv;
 			prv->prev = tmp;
